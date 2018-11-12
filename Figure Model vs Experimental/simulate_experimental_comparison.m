5a11cc9bcab062782fb64f17ce1b4b597651540b
Veikko Sariola <veikko.sariola@tut.fi>
2018-11-12 16:36:13 +0200

    Draw figure comparing the models to experimental data.

---
+++ Figure Model vs Experimental/simulate_experimental_comparison.m
@@ -0,0 +1,22 @@
+function simulate_experimental_comparison
+    a = 0.5e-3; % Radius of the probe disk, in m (SI-units)
+    gamma = 72e-3; % Surface tension, in N/m (SI-units)
+    F = 15e-6; % N
+    g = 9.81; % m/s^2
+    rho = 1000; % kg/m^3
+    deltah = 1.2e-6; % m
+
+    % Calculate volume from the weight, density and gravitational constant
+    V = F/(g*rho);
+    Vt = V/a^3;
+        
+    r = exp(linspace(log(5e-6),log(800e-6)));    
+    k = nthroot(3*Vt/pi + sqrt(1 + (3*Vt/pi)^2),3);
+    h = (k - 1 / k) * a + deltah;
+        
+    F = gamma * par_fun(@(x) drop.create_rr(h,V,x,a).force,r)';   
+    
+    if ~exist('output','dir')
+        mkdir('output');
+    end
+    save('output/r_vs_snapin.mat','gamma','r','a','F','h','V','h');
