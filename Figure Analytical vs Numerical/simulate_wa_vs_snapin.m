ee1073e924bf091e7d5755557800a188cc72f04d
Veikko Sariola <veikko.sariola@tut.fi>
2019-03-22 18:11:26 +0200

    Continue the numerical lines in the work of adhesion vs snap-in plot a bit further.

--- Figure Analytical vs Numerical/simulate_wa_vs_snapin.m
+++ Figure Analytical vs Numerical/simulate_wa_vs_snapin.m
@@ -5,7 +5,7 @@ function simulate_wa_vs_snapin
 
     V = [5 100 2500]; % normalized    
     a1 = 179.5;
-    a2 = [70 96 114];        
+    a2 = [75 75 113.5];        
     
     hs = zeros(1,length(V));    
     F = cell(1,length(V));    
@@ -14,7 +14,15 @@ function simulate_wa_vs_snapin
         angles{j} = acosd(linspace(cosd(a1)+1,cosd(a2(j))+1,100)-1);
         k = nthroot(3*V(j)/pi + sqrt(1 + (3*V(j)/pi)^2),3);
         hs(j) = k - 1 / k;                 
-        F{j} = par_fun(@(x) drop.segment_solve(V(j),'angle',x,'radius',1,'height',hs(j)).force,angles{j})';        
+        d = drop.segment_solve(V(j),'angle',angles{j}(1),'radius',1,'height',hs(j));
+        data = zeros(size(angles{j}));
+        for i = 1:length(angles{j})            
+            d = d.solve('height',hs(j),'volume',V(j),'angle1',angles{j}(i),'radius2',1);
+            data(i) = d.force;
+            d.show(); drawnow;     
+            fprintf('%f %f %f %f %f\n',angles{j}(i),d.params);
+        end
+        F{j} = data;        
     end
     if ~exist('output','dir')
         mkdir('output');
